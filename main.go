9a00176374af0784a3f17ea2404aca69d495d1c7
Mathieu AUBRY <m.aubry130@gmail.com>
2026-07-18 13:55:23 +0200

    go routine + buffering + parsing Josn

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"encoding/json"
 	"log"
 
 	"github.com/coder/websocket"
@@ -10,7 +10,34 @@ import (
 
 const wsendpoint = "wss://stream.binance.com:9443/ws/btcusdt@aggTrade"
 
+type AggTrade struct {
+	EventType string `json:"e"`
+	EventTime int64  `json:"E"`
+	Symbol    string `json:"s"`
+	Price     string `json:"p"`
+	Quantity  string `json:"q"`
+	TradeTime int64  `json:"T"`
+}
+
+func readStream(ctx context.Context, c chan []byte, conn *websocket.Conn) {
+	for {
+		_, result, err := conn.Read(ctx)
+		if err != nil {
+			log.Fatalf("Read Error %v", err)
+			return
+		}
+		select {
+		case c <- result:
+		default:
+			log.Println("Message drop")
+		}
+
+	}
+}
+
 func main() {
+	var aggTrade AggTrade
+	channel := make(chan []byte, 128)
 	ctx := context.Background()
 	conn, _, err := websocket.Dial(ctx, wsendpoint, nil)
 	if err != nil {
@@ -18,13 +45,12 @@ func main() {
 	}
 	defer conn.Close(websocket.StatusNormalClosure, "Client closing connection")
 
-	for {
-		_, result, err := conn.Read(ctx)
+	go readStream(ctx, channel, conn)
+	for msg := range channel {
+		err := json.Unmarshal(msg, &aggTrade)
 		if err != nil {
-			log.Fatalf("Read Error %v", err)
-			return
+			log.Printf("Json Parsing Error %v", err)
 		}
-		fmt.Println(string(result))
+		log.Println(aggTrade)
 	}
-
 }
