bafddc9242da64824f24cc6efa4556ee1508f856
Mathieu AUBRY <m.aubry130@gmail.com>
2026-07-23 00:21:20 +0200

    add metric ingest_message_receive_total for SLI on the ration 1 - (receive/drop)

--- main.go
+++ main.go
@@ -23,13 +23,14 @@ type AggTrade struct {
 	TradeTime int64  `json:"T"`
 }
 
-func readStream(ctx context.Context, c chan []byte, conn *websocket.Conn, messageDropped prometheus.Counter) {
+func readStream(ctx context.Context, c chan []byte, conn *websocket.Conn, messageDropped prometheus.Counter, messageReceive prometheus.Counter) {
 	for {
 		_, result, err := conn.Read(ctx)
 		if err != nil {
 			log.Fatalf("Read Error %v", err)
 			return
 		}
+		messageReceive.Inc()
 		select {
 		case c <- result:
 		default:
@@ -60,7 +61,12 @@ func main() {
 			Name: "ingest_message_dropped_total",
 			Help: "The total number of message dropped.",
 		})
-	prometheus.MustRegister(metricDuration, messageDropped)
+	messageReceive := prometheus.NewCounter(
+		prometheus.CounterOpts{
+			Name: "ingest_message_receive_total",
+			Help: "The total number of message ingested.",
+		})
+	prometheus.MustRegister(metricDuration, messageDropped, messageReceive)
 	ctx := context.Background()
 	conn, _, err := websocket.Dial(ctx, wsendpoint, nil)
 	if err != nil {
@@ -68,7 +74,7 @@ func main() {
 	}
 	defer conn.Close(websocket.StatusNormalClosure, "Client closing connection")
 
-	go readStream(ctx, channel, conn, messageDropped)
+	go readStream(ctx, channel, conn, messageDropped, messageReceive)
 	go httpPrometheusExporter()
 	for msg := range channel {
 		pipelineStart := time.Now()
@@ -78,7 +84,6 @@ func main() {
 		}
 		metricDuration.WithLabelValues("parse").Observe(time.Since(pipelineStart).Seconds())
 		processingStart := time.Now()
-		log.Println(aggTrade)
 		metricDuration.WithLabelValues("processing").Observe(time.Since(processingStart).Seconds())
 		metricDuration.WithLabelValues("pipeline").Observe(time.Since(pipelineStart).Seconds())
 	}
